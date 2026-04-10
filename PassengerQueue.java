4db3225e613779d5534adc1e1a24864e9a96a32e
Baku26 <bakunasa@go.stockton.edu>
2026-04-09 21:48:18 -0400

    Refactor PassengerQueue class and remove unused code

--- PassengerQueue.java
+++ PassengerQueue.java
@@ -1,77 +1,38 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
-import java.util.NoSuchElementException;
 
 public class PassengerQueue {
-private LinkedList<Passenger> Queue;  
-private Queue<Passenger> theQueue;
-private int numServed;
-private double totalWait;
-private String queueName;
-private int ArrivalTime;
-private double arrivalRate;
-private int size;
 
-
-public boolean isEmpty() {
-    return Queue.isEmpty();
+    private Queue<Passenger> queue = new LinkedList<>();
+    private String name;
+    private int served = 0;
+    private int totalWait = 0;
     
+    public PassengerQueue(String name) {
+        this.name = name;
+    }
+    public void add(int clock) {
+        queue.add(new Passenger(clock));
+        
+    }
+    public boolean isEmpty() {
+        return queue.isEmpty();
+        
+    }
+    public int update(int clock, boolean showAll) {
+        Passenger p = queue.poll();
+        int processingTime = new Random().nextInt(5) + 1;
+        served++;
+        totalWait += (clock - p.arrivalTime);
+        if(showAll) {
+            System.out.println("time" + clock + "serving"
+            + name + "wait is" + (clock - p.arrivalTime));
+            
+        }
+        return processingTime;
+    }  public void PrintStats() {
+            System.out.println(name + "served" + served + ", Average wait:" 
+            + (served == 0 ? 0 : (double)totalWait/served));
+ }
 }
-public int size() {
-    return Queue.size();
-    
-}
-public PassengerQueue(String queueName) {
-numServed = 0;
-totalWait = 0;
-this.queueName = queueName;
-theQueue = new LinkedList<Passenger>();
-   }
-   
-public void checkNewArrival(int clock, boolean showAll) {
-if (Math.random() < arrivalRate) {
-theQueue.add(new Passenger(clock));
-if (showAll) {
-System.out.println("Time is " + clock + ": " + queueName + " arrival, new queue size is "
-+ theQueue.size());
- 
-}
-
-}
-
-}
-public void addPassenger(Passenger p) {
-    theQueue.add(p);
-    System.out.println("added" + p.getName());
-    
- 
-}
-public Passenger removePassenger() {
-    return Queue.poll();
-}
-
-public int getArrivalTime() {
-    return ArrivalTime;
-    
-}
- 
-public int update(int clock, boolean showAll) {
-if(theQueue.isEmpty()) {
-    return 0;
-}
-Passenger nextPassenger = Queue.removeFirst();
-int timeStamp = getArrivalTime();
-int wait = clock - timeStamp;
-totalWait += wait;
-numServed++;
-
-if(showAll) {
-    System.out.println("the time is" + clock + "Serving" + queueName + "with the time stamp" + timeStamp);
-    
-}
-
-return wait;
-  }
-   }
-
