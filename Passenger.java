d1aa13b5a009fbb681172651d4c22a8cd2423a9b
Baku26 <bakunasa@go.stockton.edu>
2026-04-09 22:02:21 -0400

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,10 +1,27 @@
+
 public class Passenger
  {
 
 int arrivalTime;
+int processingTime;
+int passengerId;
 
 public Passenger(int arrivalTime) {
     this.arrivalTime = arrivalTime;
     
+}
+public int getArrivalTime() {
+return arrivalTime;
+}
+
+public int getProcessingTime() {
+return processingTime;
+}
+
+public int getId() {
+return passengerId;
+}
+public static void setMaxProcessingTime(int maxProcessTime) {
+maxProcessTime = maxProcessTime;
 }
 }
