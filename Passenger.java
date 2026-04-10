6456d9d22f79ae1c59aa89ed9247befdc3fdd4f8
Baku26 <bakunasa@go.stockton.edu>
2026-04-09 21:47:42 -0400

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,53 +1,10 @@
-import java.util.Random;
-import java.util.Queue;
-import java.util.Scanner;
-
 public class Passenger
  {
 
-private String name;  
-private int passengerId;
-private int processingTime;
-private int arrivalTime;
-private static int maxProcessingTime;
-private static int idNum = 0;
-private int service;
+int arrivalTime;
 
-Scanner sc = new Scanner(System.in);
-
-public Passenger( int passengerId, int processingTime) {
-    
-    this.passengerId = passengerId;
-    this.processingTime = processingTime;
+public Passenger(int arrivalTime) {
     this.arrivalTime = arrivalTime;
-    this.name = name;
-
+    
 }
-
-public String getName() {
-    return name;
-}
-public Passenger(int arrivalTime ) {
-this.arrivalTime = arrivalTime;
-processingTime = 2 + maxProcessingTime;
-passengerId = idNum++;
-}
-
-public int getArrivalTime() {
-return arrivalTime;
-}
-
-public int getProcessingTime() {
-return processingTime;
-}
-
-public int getId() {
-return passengerId;
-}
-
-
-public void setmaxProcessingTime(int maxProcessTime) {
-maxProcessingTime = maxProcessTime;
-}
-
 }
